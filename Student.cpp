924466c9905e5d8f7f99d2a85d204033cfb31076
orgab <isr641@gmail.com>
2019-01-09 23:52:04 +0200

    documentation for Student.cpp

--- Student.cpp
+++ Student.cpp
@@ -8,10 +8,11 @@ using std::cout;
 
 
 /*
-  Function:
-  Description:
-  Parameters:
-  Return value:
+  Function     : Student
+  Description  : constructor , for Student class, initial values for objects in at Student class
+  Parameters   : person_id     - person id
+				 p_person_name - pointer to student name
+  Return value :
 */
 Student::Student(int person_id, char* p_person_name):
 	Person(person_id, p_person_name), num_of_ee_courses_(0), num_of_cs_courses_(0)
@@ -25,20 +26,21 @@ Student::Student(int person_id, char* p_person_name):
 	}
 
 /*
-  Function:
-  Description:
-  Parameters:
-  Return value:
+  Function	   : getCourseCnt
+  Description  : return number of courses from 2 faculty : EE,CS
+  Parameters   : None
+  Return value : number of courses from 2 faculty : EE,CS
 */
 int Student::getCourseCnt() const{
 	return num_of_ee_courses_ + num_of_cs_courses_;
 }
 
 /*
-  Function:
-  Description:
-  Parameters:
-  Return value:
+  Function     : course_already_exist
+  Description  : check if course is already exist in the coyrse array of the student
+  Parameters   : p_new_course  - pointer to the new course
+				 p_exist_cours - pointer for array course of the student
+  Return value : '1' if the course already exist else 0;
 */
 int course_already_exist(Course* p_new_course,Course* p_exist_cours[] ) {
 	int i ;
@@ -52,10 +54,10 @@ int course_already_exist(Course* p_new_course,Course* p_exist_cours[] ) {
 }
 
 /*
-  Function:
-  Description:
-  Parameters:
-  Return value:
+  Function     : first_free_pointer
+  Description  : find the first index in array that in this index the value is NULL
+  Parameters   :  first_array_pointer - pointer to array
+  Return value : if array is full return -1 else return the index 
 */
 int first_free_pointer(Course* first_array_pointer[]) {
 	int i ;
@@ -66,10 +68,10 @@ int first_free_pointer(Course* first_array_pointer[]) {
 }
 
 /*
-  Function:
-  Description:
-  Parameters:
-  Return value:
+  Function     : addEE_Course
+  Description  : add EE_Course for a student
+  Parameters   : p_ee_course -pointer for EE_Course class
+  Return value : '1' if succssed to add the course else '0' 
 */
 bool Student::addEE_Course(EE_Course* p_ee_course) {
 	if (p_ee_course == NULL) return 0;
@@ -90,10 +92,10 @@ bool Student::addEE_Course(EE_Course* p_ee_course) {
 }
 
 /*
-  Function:
-  Description:
-  Parameters:
-  Return value:
+  Function     : addCS_Course
+  Description  : add CS_Course for a student
+  Parameters   : p_cs_course -pointer for CS_Course class
+  Return value : '1' if succssed to add the course else '0'
 */
 bool Student::addCS_Course(CS_Course* p_cs_course) {
 	if (p_cs_course == NULL) return 0;
@@ -114,10 +116,10 @@ bool Student::addCS_Course(CS_Course* p_cs_course) {
 }
 
 /*
-  Function:
-  Description:
-  Parameters:
-  Return value:
+  Function     : getEE_Course
+  Description  : get a pointer for specific  EE_Course according to parameter 
+  Parameters   : ee_course_num- course number
+  Return value : return the pointer if the course exist else NULL
 */
 EE_Course* Student::getEE_Course(int ee_course_num) {
 	int i;
@@ -129,41 +131,35 @@ EE_Course* Student::getEE_Course(int ee_course_num) {
 }
 
 /*
-  Function:
-  Description:
-  Parameters:
-  Return value:
+  Function     : getCS_Course
+  Description  : get a pointer for specific  CS_Course according to parameter
+  Parameters   : cs_course_num- course number
+  Return value : return the pointer if the course exist else NULL
 */
 CS_Course* Student::getCS_Course(int cs_course_num) {
 	int i ;
 	for(i=0;i<MAX_COURSE_NUM;i++) {
-		
 		if (p_cs_course_array_[i] == NULL) continue;
 		if (p_cs_course_array_[i]->getNum() == cs_course_num) return p_cs_course_array_[i];
-		
 	}
-	
 	return NULL;
 }
 
 /*
-  Function:
-  Description:
-  Parameters:
-  Return value:
+  Function    : getAvg
+  Description : return the total student avarage(includes cs courses and ee courses) 
+  Parameters  : None
+  Return value: srtudent avarage , value between 0-100
 */
 int Student::getAvg()  const {
 	int i;
 	double avg = 0,sum = 0;
 	if (getCourseCnt() == 0)return avg;
-	
-
 	for (i = 0; i < MAX_COURSE_NUM; i++) {
 		if (p_cs_course_array_[i] != NULL) {
 			sum += p_cs_course_array_[i]->getCourseGrade();	
 		}
 		if (p_ee_course_array_[i] != NULL) {
-			
 			sum += p_ee_course_array_[i]->getCourseGrade();
 		}
 	}
@@ -172,10 +168,10 @@ int Student::getAvg()  const {
 }
 
 /*
-  Function:
-  Description:
-  Parameters:
-  Return value:
+  Function     : rem_Course
+  Description  : remove a specific course from the list of student's course
+  Parameters   : course_num - course number to remove
+  Return value : '1' if succsses to remove else '0'
 */
 bool Student::rem_Course(int course_num) {
 	int i;
@@ -201,10 +197,10 @@ bool Student::rem_Course(int course_num) {
 }
 
 /*
-  Function:
-  Description:
-  Parameters:
-  Return value:
+  Function     : print
+  Description  : print information about a student grades and courses
+  Parameters   : None
+  Return value : None
 */
 void Student::print() const {
 	char* p_student_name = getName();
@@ -235,6 +231,15 @@ void Student::print() const {
 }
 
 
+
+
+
+/*
+  Function     : ~Student
+  Description  : destructor ,remove all the allocated courses 
+  Parameters   : None
+  Return value : None
+*/
 Student::~Student()
 {
 	int i ;
